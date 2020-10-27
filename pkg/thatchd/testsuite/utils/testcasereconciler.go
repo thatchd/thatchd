e419444e2d2c1d50a77b5684d400d86419d6b2c1
Sergio Franco <sfrancog@redhat.com>
2020-10-27 21:50:33 +0000

    feat: Add utility test case state reconciler
    Create utility test suite state reconciler that reconciles the current
    test cases.

---
+++ pkg/thatchd/testsuite/utils/testcasereconciler.go
@@ -0,0 +1,56 @@
+package utils
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	thatchdv1alpha1 "github.com/sergioifg94/thatchd/api/v1alpha1"
+	"github.com/sergioifg94/thatchd/pkg/thatchd/testsuite"
+	k8sclient "sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// TestCaseState represents the curent state of a TestCase
+type TestCaseState struct {
+	Name        string                                `json:"name"`
+	Annotations map[string]string                     `json:"annotations,omitempty"`
+	Status      thatchdv1alpha1.TestCaseCurrentStatus `json:"status,omitempty"`
+}
+
+// TestCaseReconciler is a state reconciler for the TestCaseState. It reconciles
+// the state from the current test cases in the namespace
+type TestCaseReconciler struct{}
+
+var _ testsuite.Reconciler = &TestCaseReconciler{}
+
+func NewTestCaseReconciler() *TestCaseReconciler {
+	return &TestCaseReconciler{}
+}
+
+func (r *TestCaseReconciler) ParseState(state string) (interface{}, error) {
+	result := []TestCaseState{}
+	if err := json.Unmarshal([]byte(state), &result); err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func (r *TestCaseReconciler) Reconcile(client k8sclient.Client, namespace string, currentState interface{}) (interface{}, error) {
+	testCaseList := &thatchdv1alpha1.TestCaseList{}
+	if err := client.List(context.TODO(), testCaseList, k8sclient.InNamespace(namespace)); err != nil {
+		return nil, fmt.Errorf("failed to list test cases: %w", err)
+	}
+
+	result := make([]TestCaseState, 0, len(testCaseList.Items))
+
+	for _, testCase := range testCaseList.Items {
+		result = append(result, TestCaseState{
+			Name:        testCase.Name,
+			Annotations: testCase.Annotations,
+			Status:      testCase.Status.Status,
+		})
+	}
+
+	return result, nil
+}
---
+++ pkg/thatchd/testsuite/utils/testcasereconciler_test.go
@@ -0,0 +1,66 @@
+package utils
+
+import (
+	"reflect"
+	"testing"
+
+	thatchdv1alpha1 "github.com/sergioifg94/thatchd/api/v1alpha1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+)
+
+func TestTestCaseReconciler(t *testing.T) {
+	scenarios := []struct {
+		Name          string
+		TestCases     []runtime.Object
+		CurrentState  []TestCaseState
+		ExpectedState []TestCaseState
+	}{
+		{
+			Name: "Test cases reconciled",
+			TestCases: []runtime.Object{
+				&thatchdv1alpha1.TestCase{
+					ObjectMeta: v1.ObjectMeta{
+						Name:      "test-case1",
+						Namespace: "thatchd",
+						Annotations: map[string]string{
+							"test-component": "component1",
+						},
+					},
+					Status: thatchdv1alpha1.TestCaseStatus{
+						Status: thatchdv1alpha1.TestCaseDispatched,
+					},
+				},
+			},
+			CurrentState: []TestCaseState{},
+			ExpectedState: []TestCaseState{
+				{
+					Name: "test-case1",
+					Annotations: map[string]string{
+						"test-component": "component1",
+					},
+					Status: thatchdv1alpha1.TestCaseDispatched,
+				},
+			},
+		},
+	}
+
+	for _, scenario := range scenarios {
+		t.Run(scenario.Name, func(t *testing.T) {
+			scheme := runtime.NewScheme()
+			thatchdv1alpha1.AddToScheme(scheme)
+			client := fake.NewFakeClientWithScheme(scheme, scenario.TestCases...)
+
+			reconciler := NewTestCaseReconciler()
+			state, err := reconciler.Reconcile(client, "thatchd", scenario.CurrentState)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if !reflect.DeepEqual(state, scenario.ExpectedState) {
+				t.Errorf("unexpected reconciled state value. Expected %v, but got %v", scenario.ExpectedState, state)
+			}
+		})
+	}
+}
